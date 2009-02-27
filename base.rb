ef16e3a18138837e6af8a1c5202affacec9af612
danny <danny@thescri.be>
2009-02-27 14:21:26 +0000

    changed filenames

---
+++ base.rb
@@ -0,0 +1,63 @@
+#Clear out todo and generate a nifty layout
+run "echo TODO > README"
+generate :nifty_layout
+ 
+#Install gems
+gem 'RedCloth', :lib => 'redcloth'
+gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+rake "gems:install", :sudo => true
+ 
+#Initiate git repo & add gitignore rules
+git :init
+ 
+file ".gitignore", <<-END
+.DS_Store
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+END
+ 
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "cp config/database.yml config/example_database.yml"
+ 
+#Add initial stuff to git repo
+git :add => ".", :commit => "-m 'initial commit'"
+ 
+#Add nifty authentication if needed
+if yes?("Do you want authentication?")
+  name = ask("What do you want a user to be called?")
+  generate :nifty_authentication, name
+  rake "db:migrate"
+ 
+  git :add => ".", :commit => "-m 'adding authentication'"
+end
+ 
+#Remove prototype files
+  git :rm => "public/javascripts/controls.js"
+  git :rm => "public/javascripts/dragdrop.js"
+  git :rm => "public/javascripts/effects.js"
+  git :rm => "public/javascripts/prototype.js"
+  
+inside('public/javascripts') do  
+  #and add jquery
+  run "cp ~/current_libs/jquery/* ."
+  git :add => ".", :commit => "-m 'switching prototype for jquery'"
+end
+ 
+#Vendor rails
+if yes?("Vendor rails?")
+  inside('vendor') do
+    run "git clone git://github.com/rails/rails.git"
+  end
+end
+ 
+#Generate welcome controller for front page
+generate :controller, "welcome index"
+route "map.root :controller => 'welcome'"
+git :rm => "public/index.html"
+git :add => ".", :commit => "-m 'adding welcome controller'"
+ 
+ 
+#Notes
+puts "\n\nRemember to set up the passenger prefpane!\n"
